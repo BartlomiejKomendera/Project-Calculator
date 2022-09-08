b5023b6d3901ea49c3472b87057865de41bf2f73
Bartek <bartekkomendera@gmail.com>
2022-09-08 13:05:15 +0200

    Created all of the main functions.

---
+++ calculator.js
@@ -0,0 +1,25 @@
+function operate(firstNumber, secondNumber, operator) {
+    switch (operator) {
+        case "+":
+            return add(firstNumber, secondNumber);
+        case "-":
+            return substract(firstNumber, secondNumber);
+        case "*":
+            return multiply(firstNumber, secondNumber);
+        case "/":
+            return divide(firstNumber, secondNumber);
+    }
+}
+
+function add(firstNumber, secondNumber) {
+    return firstNumber + secondNumber;
+}
+function substract(firstNumber, secondNumber) {
+    return firstNumber - secondNumber;
+}
+function multiply(firstNumber, secondNumber) {
+    return firstNumber * secondNumber;
+}
+function divide(firstNumber, secondNumber) {
+    return firstNumber / secondNumber;
+}
